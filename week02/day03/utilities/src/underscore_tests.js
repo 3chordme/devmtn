f6f8ddf746a850927741af2e4f6a045fbfa6f908
David Moore <3chordme@gmail.com>
2015-10-21 18:34:52 -0600

    Partial solution problem utilities #4

--- week02/day03/utilities/src/underscore_tests.js
+++ week02/day03/utilities/src/underscore_tests.js
@@ -46,11 +46,27 @@ var _ = { };
   // Call iterator(value, key, collection) for each element of collection.
   // Accepts both arrays and objects.
   _.each = function(collection, iterator) {
+    if (Array.isArray(collection) !== true) {
+      for (var prop in collection) {
+        iterator(collection[prop], prop, collection);
+      }
+    }
+    else {
+      for (var i = 0; i < iterator.length; i++) {
+        iterator(collection[i], i, collection);
+      }
+    }
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
   // is not present in the array.
   _.indexOf = function(array, target){
+    for (i = 0; i < array.length; i++) {
+      if (array[i] === target) {
+        return i;
+      }
+    }
+    return -1;
   };
 
   // Return all elements of an array that pass a truth test.
