ce1e76a028294a9a2823457d6611eb254c7daebe
David Moore <3chordme@gmail.com>
2015-10-23 15:49:34 -0600

    char-count changes back to #999 if > 10, comments added

--- week02/day05/twitter-clone/js/animations.js
+++ week02/day05/twitter-clone/js/animations.js
@@ -12,6 +12,10 @@ $(document).ready(function() {
     $('.tweet-compose').keyup(function(key) {
       //if not delete keycode == 43
       //WRONG in mac it is keyCode 8
+      //doesn't work if you hold down delete or highlight and delete or paste into.
+      //use $(this).val().length(); maybe
+
+
         if (key.keyCode !== 8) {
           //console.log(key.keyCode)
           charactersNum -= 1;
@@ -21,14 +25,21 @@ $(document).ready(function() {
           charactersNum += 1;
           $('#char-count').text(charactersNum.toString());
         }
+
         if (charactersNum <= 10) {
           $('#char-count').css('color', 'red');
         }
+
         if (charactersNum < 0) {
           $('#tweet-submit').hide();
         }
+
         if (charactersNum > 0) {
           $('#tweet-submit').show();
+        }
+
+        if (charactersNum > 10) {
+          $('#char-count').css('color', '#999');
         }
 
     });
