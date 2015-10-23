bc10735639dccadacb80e122c38bcc377bb4a1ed
David Moore <3chordme@gmail.com>
2015-10-23 16:01:29 -0600

    Changed button hiding to button disabled

--- week02/day05/twitter-clone/js/animations.js
+++ week02/day05/twitter-clone/js/animations.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
     var charactersNum = parseInt($('#char-count').text());
     //console.log(charactersNum);
 
-    $('.tweet-compose').keyup(function(key) {
+    $('.tweet-compose').keydown(function(key) {
       //if not delete keycode == 43
       //WRONG in mac it is keyCode 8
       //doesn't work if you hold down delete or highlight and delete or paste into.
@@ -31,11 +31,11 @@ $(document).ready(function() {
         }
 
         if (charactersNum < 0) {
-          $('#tweet-submit').hide();
+          $('#tweet-submit').attr('disabled', true);
         }
 
         if (charactersNum > 0) {
-          $('#tweet-submit').show();
+          $('#tweet-submit').attr('disabled', false);
         }
 
         if (charactersNum > 10) {
