740c2311887d53fa96e004bf5a692088a65e4836
David Moore <3chordme@gmail.com>
2015-10-29 13:18:53 -0600

    Added some console logs trying to figure out how promises work in mini-userProfiles3. Commented them out.

--- week03/day04/mini-userProfiles3/service.js
+++ week03/day04/mini-userProfiles3/service.js
@@ -3,15 +3,19 @@ var app = angular.module('userProfiles');
 app.service('mainService', function($http, $q) {
   this.getUsers = function() {
     var deferred = $q.defer();
+    //console.log(deferred.promise);
     $http({
         method: 'GET',
         url: 'http://reqres.in/api/users?page=1'
     }).then(function(response){
+      //console.log(deferred.promise);
       var parsedResponse = response.data.data;
       for (var i = 0; i < parsedResponse.length; i++){
         parsedResponse[i].first_name = 'Ralf'
       }
+        //console.log(deferred.promise);
         deferred.resolve(parsedResponse);
+        //console.log(deferred.promise);
     });
     return deferred.promise;
   }
