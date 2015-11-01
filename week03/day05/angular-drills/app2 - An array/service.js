def501bbf75a04088c39c8854cb5fe7a8f836c3a
David Moore <3chordme@gmail.com>
2015-11-01 16:53:33 -0700

    app1 attempt 2 done

--- week03/day05/angular-drills/app2 - An array/service.js
+++ week03/day05/angular-drills/app2 - An array/service.js
@@ -101,13 +101,16 @@ var myData = [
 	{"name": "Odysseus Nash", "email": "purus@rutrum.co.uk", "dateEmployed": "01/08/2016", "phone": "722-7908"}
 ];
 
+angular.module('arrayApp').service('myServices', function() {
 
+	var myArray = [];
 
+	for (i = 0; i < myData.length; i++) {
+		myArray.push(myData[i]);
+	}
 
+	this.dataService = function() {
+		return myArray;
+	};
 
-
-
-
-
-
-
+});
