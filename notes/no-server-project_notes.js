aa956d49efd4833f6874e68ece7c1c28d23ace31
David Moore <3chordme@gmail.com>
2015-11-08 21:48:46 -0700

    Yet again, unsure of changes.

--- notes/no-server-project_notes.js
+++ notes/no-server-project_notes.js
@@ -1,43 +1,11 @@
 NO SERVER PROJECT NOTES
 
-At least 3 routes, aka pages. One page per day.
+[x] At least 3 routes, aka pages. One page per day.
 
-At least 2 directives. One can be a template url. The other has to do some javascript.
+[ ] At least 2 directives. One can be a template url. The other has to do some javascript.
 
-Using firebase or an api is a bonus. Only do it if you get the rest finished.
+[ ] Using firebase or an api is a bonus. Only do it if you get the rest finished.
 
-Present to class Thursday and Friday afternoon 1:30-2:30 pm.
+[ ] Present to class Thursday and Friday afternoon 1:30-2:30 pm.
 
 Mindmaps and orm to keep track of where you are.
-
-
-IDEAS:
-
-(WEARTHER):
-What to Wear Weather App:
-
-  -> Record: What temp is it, what are you wearing, are you too hot or too cold, comments
-  -> Log(Previous weathers and clothing, too hot or too cold)
-  -> Predict(What temp is it, what should you wear)
-
-(MEMORI)
-Pi Memorization Game:
-
-  -> Home (New Game, Scores)
-  -> Play (Entry box, what you have entered, if you get it wrong you lose a life, lose 3 life game over, enter 10 correct
-  and gain a life.)
-  -> Top Scores
-
-(NEWTA)
-Next train app.
-
-  -> Home (Hit a button, tells you what train is leaving next based on time)
-  -> Previous Trains (What trains time/day you have taken before)
-  -> Schedule (displays weekday or weekend northbound train schedule)
-
-(DAVIDCOLLINSMOORE.COM)
-Portfolio website
-
-  -> Home (Introduction)
-  -> Resume (Current resume + link to portfolio)
-  -> Contact (Contact info plus send message form)
